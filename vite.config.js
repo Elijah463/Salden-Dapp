0e3d7253b398b0a0352f5cd26a18c1d3e72a163a
Crypto-boi <eelijah463@gmail.com>
2026-03-17 13:51:52 +0100

    Remove manualChunks to fix initialization crash

--- vite.config.js
+++ vite.config.js
@@ -16,21 +16,6 @@ export default defineConfig({
     include: ["buffer"],
   },
   build: {
-    rollupOptions: {
-      output: {
-            manualChunks: (id) => {
-      if (id.includes("node_modules/react") || id.includes("node_modules/react-dom") || id.includes("node_modules/react-router-dom")) {
-        return "vendor";
-      }
-      if (id.includes("node_modules/thirdweb")) {
-        return "thirdweb";
-      }
-      if (id.includes("node_modules/ethers")) {
-        return "ethers";
-      }
-    },
-  },
-},
     sourcemap: false,
     minify: true,
   },
